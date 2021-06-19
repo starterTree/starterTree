8eadc6a4eb648100b2dc1c934ea43b3ecee2770c
Thomas Roche <thomas.roche@inrae.fr>
2021-06-19 17:41:01 +0200

    add support icon

--- starterTree.py
+++ starterTree.py
@@ -30,6 +30,7 @@ keyword_module_opn="opn"
 keyword_module_ssh="ssh"
 modules=""
 absolute_path_main_config_file=os.path.dirname(file_main)+"/"
+listIcon=['','','','','']
 def my_fun(source_dict,menu_completion,path_entry_name):
 	for key in source_dict:
 		path_entry_name_content["path_"+path_entry_name+key]={}
@@ -42,16 +43,19 @@ def my_fun(source_dict,menu_completion,path_entry_name):
 			if not isinstance(source_dict[key][subKey],dict):
 				icon=""
 				if subKey == keyword_module_opn:
-					icon=""
+					icon=""
 				if subKey == keyword_module_ssh:
-					icon=""
+					icon=""
+				if subKey == keyword_module_cmd:
+					icon=""
 				path_entry_name_content["path_"+path_entry_name+key][subKey]=source_dict[key][subKey]
 				print(subKey)
-				menu_completion[key+icon]=None
+				menu_completion[icon+key]=None
 			
 			if isinstance(source_dict[key][subKey], dict):
-				menu_completion[key]={}
-				my_fun(source_dict[key], menu_completion[key] ,path_entry_name+key)
+				icon=""
+				menu_completion[icon+key]={}
+				my_fun(source_dict[key], menu_completion[icon+key] ,path_entry_name+key)
 				
 menu_completion={}
 my_fun(yaml.load(open(file_main, 'r'),Loader=yaml.SafeLoader),menu_completion,"")
@@ -71,6 +75,10 @@ def main():
 	session = PromptSession(os.path.basename(sys.argv[0])+u" > ", completer=completer, mouse_support=True, complete_style=CompleteStyle.MULTI_COLUMN,key_bindings=bindings)
 	try:
 		prompt_id=session.prompt(pre_run=session.default_buffer.start_completion,).replace(" ","")
+		historyName=prompt_id
+		for i in prompt_id:
+			if i in listIcon:
+				prompt_id=prompt_id.replace(i,"")
 	except:
 		exit()
 	prompt_id="path_"+prompt_id 
@@ -79,7 +87,7 @@ def main():
 			text = "xdg-open "+path_entry_name_content[prompt_id][keyword_module_opn]
 			os.system(text)   
 			with open(os.environ['HOME']+"/.bash_history", "a") as myfile:
-				myfile.write(text+' # '+prompt_id+'\n')
+				myfile.write(text+' # '+historyName+'\n')
 		if keyword_module_cmd in path_entry_name_content[prompt_id] or keyword_module_cmd_c in path_entry_name_content[prompt_id]:
 			if  keyword_module_cmd_c in path_entry_name_content[prompt_id]:
 				text = path_entry_name_content[prompt_id][keyword_module_cmd_c]
@@ -90,7 +98,7 @@ def main():
 				exit()
 			os.system(text)
 			with open(os.environ['HOME']+"/.bash_history", "a") as myfile:
-				myfile.write(text+' # '+prompt_id+'\n')
+				myfile.write(text+' # '+historyName+'\n')
 
 
 	else:
