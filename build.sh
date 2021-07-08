d95e6daf37a81465d4d264abaf5a419ff03aef7f
Thomas Roche <thomas.roche@inrae.fr>
2021-07-08 19:51:02 +0200

    up build

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 [[ $1 == "" ]] && { echo need version as '$1' ; exit 1 ; }
 sed -i "s/version is.*hash/version is $(git rev-parse HEAD) hash/g" starterTree.py
 sed -i "s/version is version/$1/g" starterTree.py
-version is version
 rm -rf ~/build/*
 cxfreeze -c starterTree.py --target-dir ~/build/starterTree
 cd ~/build/ && tar -zcvf starterTree.tar.gz starterTree && cd -
