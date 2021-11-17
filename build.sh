cf9c3a6c7ff77fd73e916523ae7fa8ed01662486
Thomas Roche <thomas.roche@inrae.fr>
2021-11-17 22:21:42 +0100

    up

--- build.sh
+++ build.sh
@@ -1,12 +1,17 @@
+nameScript=starterTree.py
+nameBinary=st
 [[ $1 == "" ]] && { echo need version as '$1' ; exit 1 ; }
-sed -i "s/version is.*hash/version is $(git rev-parse HEAD) hash/g" starterTree.py
-sed -i "s/version is version/$1/g" starterTree.py
-rm -rf ~/build/*
-cxfreeze -c starterTree.py --target-dir ~/build/starterTree
-cd ~/build/ && tar -zcvf starterTree.tar.gz starterTree && cd -
-ln -sf ~/build/starterTree/starterTree ~/.local/bin/st-build
-sed -i "s/version is.*hash/version is git rev-parse HEAD hash/g" starterTree.py
-sed -i "s/$1/version is version/g" starterTree.py
+sed -i "s/version is.*hash/version is $(git rev-parse HEAD) hash/g" $nameScript
+sed -i "s/version is version/$1/g" $nameScript
+mkdir -p ./build
+rm -rf ./build/*
+cxfreeze -c $nameScript --target-dir ./build/$nameBinary
+cd ./build/ && tar -zcvf $nameBinary.tar.gz $nameBinary && cd -
+ln -sf ./build/$nameBinary/$nameBinary ~/.local/bin/$nameBinary
+sed -i "s/version is.*hash/version is git rev-parse HEAD hash/g" $nameScript
+sed -i "s/$1/version is version/g" $nameScript
+
+
 
 
 
